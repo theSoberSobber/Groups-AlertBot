a79fc782414125f2cb2f68c51b2af99fd3837da5
theSoberSobber <akshatshrivastava0010@gmail.com>
2022-10-11 10:38:16 +0530

    ok

--- main.js
+++ main.js
@@ -4,8 +4,13 @@ const app = express();
 
 app.listen(process.env.PORT || 3000);
 // _____________________________________
+app.get('/', async (req, res) => {
+    res.status(200).send('AlertBot@2022');
+})
+
 app.get('/group', async (req, res) => {
-    require('./group.js')();
+    // require('./group.js')();
+    res.send('Ok');
 })
 
 app.get('/groupIds', async (req, res) => {
